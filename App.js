2b8767e0313cbb618a4ef0b63aced550704e8128
mzeeshansikander <zeeshan.sikander@live.com>
2022-09-06 17:09:58 +0500

    working on a new project

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ const port=process.env.PORT || 3000;
 
 //Routing
 app.get("/",(req,res)=>{
-    res.send("hello") //html
+    // res.send("hello") //html
+    res.render("index") //we cannot render the file only we have to configure it too as express doesnt konw anything about hbs
+    
+
 })
 
 //create Server
